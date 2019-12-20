d30bfc16503cf65519313a048f79883c93cebd36
Ankit <ankitnashier91@gmail.com>
2019-12-20 21:54:48 +0530

    http requests log added

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import 'source-map-support/register'
-
 import express from 'express';
+import logger from 'morgan';
 
 const app = express();
+
+app.use(logger('dev'))
 
 app.get('/', (req, res) => {
   res.send("ok")
